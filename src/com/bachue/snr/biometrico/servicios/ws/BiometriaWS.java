a6d881a8c257c84f6ccc26450cf4d6a1e737875c
Nox <juan.angarita.11@gmail.com>
2019-08-22 23:25:23 -0500

    Something was made.

---
+++ src/com/bachue/snr/biometrico/servicios/ws/BiometriaWS.java
@@ -0,0 +1,63 @@
+package com.bachue.snr.biometrico.servicios.ws;
+
+import com.bachue.snr.biometrico.admon.facade.ejb.stateless.IHuellaBusiness;
+import com.bachue.snr.biometrico.admon.facade.ejb.stateless.ILogBusiness;
+import com.bachue.snr.biometrico.admon.facade.ejb.stateless.ISesionBusiness;
+import com.bachue.snr.biometrico.admon.facade.ejb.stateless.IUsuarioBusiness;
+import com.bachue.snr.biometrico.admon.persistence.dto.SesionDTO;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
+import javax.jws.WebService;
+
+/**
+ *
+ * @version 1.0
+ * @author Barras y Recaudos.
+ * Nota: Servicio SOAP con endpoints para el proyecto de biometria.
+ *
+ */
+@Stateless
+@WebService(serviceName = "BiometriaController")
+public class BiometriaWS {
+
+  @EJB
+  ISesionBusiness iisb_sesionBusiness;
+
+  @EJB
+  IHuellaBusiness iihb_huellaBusiness;
+
+  @EJB
+  ILogBusiness iilb_logBusiness;
+
+  @EJB
+  IUsuarioBusiness iiub_usuarioBusiness;
+
+  /**
+   * Metodo que recibe la peticion de verificacion de sesion.
+   *
+   * @param as_sesion sesion que sera validada.
+   * @return DTO con la sesion y su estado.
+   */
+  @WebMethod(action = "consultarSesion")
+  @WebResult(name = "sesion")
+  public SesionDTO consultarSesion(@WebParam(name = "sesion") String as_sesion) {
+    return iisb_sesionBusiness.consultarSesion(as_sesion);
+  }
+
+
+  /**
+   * Metodo que recibe la peticion de estadisticas.
+   * @param as_tipo tipo de stat consultada.
+   * @param as_id id de la entidad asociada.
+   * @return DTO con la sesion y su estado.
+   */
+  @WebMethod(action = "consultarEstadisticas")
+  @WebResult(name = "estadisticas")
+  public int consultarStats(@WebParam(name = "tipo" ) String as_tipo, @WebParam(name = "id" ) String as_id) {
+    return iilb_logBusiness.consultarStats(as_tipo, as_id);
+  }
+}
