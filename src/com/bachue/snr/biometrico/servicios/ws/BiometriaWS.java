1032dc12ec03f1b31926ead65fa93f5ead0bca06
Nox <juan.angarita.11@gmail.com>
2019-08-24 22:09:25 -0500

    Implementation prototype

--- src/com/bachue/snr/biometrico/servicios/ws/BiometriaWS.java
+++ src/com/bachue/snr/biometrico/servicios/ws/BiometriaWS.java
@@ -4,14 +4,20 @@ import com.bachue.snr.biometrico.admon.facade.ejb.stateless.IHuellaBusiness;
 import com.bachue.snr.biometrico.admon.facade.ejb.stateless.ILogBusiness;
 import com.bachue.snr.biometrico.admon.facade.ejb.stateless.ISesionBusiness;
 import com.bachue.snr.biometrico.admon.facade.ejb.stateless.IUsuarioBusiness;
+import com.bachue.snr.biometrico.admon.persistence.dto.HuellaDTO;
 import com.bachue.snr.biometrico.admon.persistence.dto.SesionDTO;
 
+import javax.annotation.Resource;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
+import javax.servlet.http.HttpServletRequest;
+import javax.xml.ws.WebServiceContext;
+import javax.xml.ws.handler.MessageContext;
+import javax.servlet.ServletContext;
 
 /**
  *
@@ -36,6 +42,10 @@ public class BiometriaWS {
   @EJB
   IUsuarioBusiness iiub_usuarioBusiness;
 
+  @Resource
+  private WebServiceContext context;
+
+
   /**
    * Metodo que recibe la peticion de verificacion de sesion.
    *
@@ -57,7 +67,30 @@ public class BiometriaWS {
    */
   @WebMethod(action = "consultarEstadisticas")
   @WebResult(name = "estadisticas")
-  public int consultarStats(@WebParam(name = "tipo" ) String as_tipo, @WebParam(name = "id" ) String as_id) {
+  public int consultarEstadisticas(@WebParam(name = "tipo" ) String as_tipo, @WebParam(name = "id" ) String as_id) {
     return iilb_logBusiness.consultarStats(as_tipo, as_id);
+  }
+
+
+  /**
+   * Metodo que recibe la peticion HTTP de enrolamiento y la mapea al DTO.
+   * @param ahd_huellas DTO con la informacion de las huellas.
+   * @return respuesta HTTP con el resultado del enrolamiento.
+   */
+  @WebMethod(action = "enrolarUsuario")
+  @WebResult(name = "resultado")
+  public Boolean enrolarUsuario(@WebParam(name = "huellas") HuellaDTO[] ahd_huellas) {
+
+    MessageContext mc = context.getMessageContext();
+    HttpServletRequest req = (HttpServletRequest)mc.get(MessageContext.SERVLET_REQUEST);
+
+    Boolean lb_estado;
+    for(HuellaDTO lhd_huella : ahd_huellas) {
+      lhd_huella.agregarValoresAuditoria(req);
+      iihb_huellaBusiness.enrolarHuella(lhd_huella);
+    }
+
+    lb_estado = iihb_huellaBusiness.crearMegaTemplate(ahd_huellas[0]);
+    return lb_estado;
   }
 }
