6397883af415177a7817f30b2fed7b7830eba93c
Nox <juan.angarita.11@gmail.com>
2019-08-28 10:16:51 -0500

    Matching Threshold

--- src/com/bachue/snr/biometrico/admon/facade/ejb/stateless/impl/HuellaBusiness.java
+++ src/com/bachue/snr/biometrico/admon/facade/ejb/stateless/impl/HuellaBusiness.java
@@ -4,13 +4,11 @@ import com.bachue.snr.biometrico.admon.facade.ejb.stateless.IHuellaBusiness;
 import com.bachue.snr.biometrico.admon.persistence.dto.BorrarHuellasDTO;
 import com.bachue.snr.biometrico.admon.persistence.dto.HuellaDTO;
 import com.bachue.snr.biometrico.admon.persistence.dto.VerificacionDTO;
-import com.bachue.snr.biometrico.admon.persistence.ejb.dao.stateless.IHuellaDAO;
-import com.bachue.snr.biometrico.admon.persistence.ejb.dao.stateless.ILogDAO;
-import com.bachue.snr.biometrico.admon.persistence.ejb.dao.stateless.ISesionDAO;
-import com.bachue.snr.biometrico.admon.persistence.ejb.dao.stateless.IUsuarioDAO;
+import com.bachue.snr.biometrico.admon.persistence.ejb.dao.stateless.*;
 import com.bachue.snr.biometrico.admon.persistence.helper.HuellaHelper;
 import com.bachue.snr.biometrico.admon.persistence.helper.LogHelper;
 import com.bachue.snr.biometrico.admon.persistence.helper.SesionHelper;
+import com.bachue.snr.biometrico.admon.persistence.model.Constante;
 import com.bachue.snr.biometrico.biometrics.Criptografia;
 import com.bachue.snr.biometrico.biometrics.Enrolador;
 import com.bachue.snr.biometrico.biometrics.MotorBiometrico;
@@ -20,6 +18,8 @@ import com.bachue.snr.biometrico.biometrics.util.Utils;
 import javax.ejb.EJB;
 import javax.ejb.Local;
 import javax.ejb.Stateless;
+import java.util.List;
+
 /**
  *
  * @version 1.0
@@ -42,6 +42,11 @@ public class HuellaBusiness implements IHuellaBusiness {
   @EJB
   private IUsuarioDAO iiud_usuarioDao;
 
+  @EJB
+  private IConstanteDAO iicd_constanteDao;
+
+  private String is_matchingThreshold = "40";
+
   @Override
   public Boolean enrolarHuella(HuellaDTO ahd_huella) {
     if(!Utils.crearImagen(ahd_huella)) {
@@ -52,7 +57,7 @@ public class HuellaBusiness implements IHuellaBusiness {
 
   @Override
   public Boolean verificarHuella(VerificacionDTO avd_verificacion) {
-    MotorBiometrico.getInstance();
+    leerConstantes();
     Verificador lv_verificador = new Verificador();
     boolean lb_resultado = lv_verificador.verificar(avd_verificacion, iihd_huellaDao.obtenerHuellas(avd_verificacion.getUsuarioId()));
     iisd_sesionDao.crearSesion(SesionHelper.createSesion(avd_verificacion, lb_resultado));
@@ -62,7 +67,7 @@ public class HuellaBusiness implements IHuellaBusiness {
 
   @Override
   public String borrarHuellas(BorrarHuellasDTO bhd_usuario) {
-    MotorBiometrico.getInstance();
+    leerConstantes();
     Enrolador le_enrolador = new Enrolador(null);
     le_enrolador.eliminarHuellas(Criptografia.encrypt(bhd_usuario.getIdUsuario()));
     iihd_huellaDao.borrarHuellas(Criptografia.encrypt(bhd_usuario.getIdUsuario()));
@@ -72,10 +77,21 @@ public class HuellaBusiness implements IHuellaBusiness {
 
   @Override
   public Boolean crearMegaTemplate(HuellaDTO ahd_huella) {
-    MotorBiometrico.getInstance();
+    leerConstantes();
     Enrolador le_enrolador = new Enrolador(ahd_huella);
     boolean lb_enrolamiento = le_enrolador.enrolarUsuario(iihd_huellaDao.obtenerHuellas(ahd_huella.getUsuarioId()));
     iild_logDao.crearEvento(LogHelper.crearLogDeEnrolamiento(ahd_huella, lb_enrolamiento));
     return lb_enrolamiento;
+  }
+
+  private void leerConstantes() {
+    List<Constante> llc_constantes = iicd_constanteDao.obtenerConstantes();
+    llc_constantes.forEach(e -> {
+      if(e.getIdConstante().equals("MATCHING_THRESHOLD_BIOMETRIA")) {
+        is_matchingThreshold = e.getCaracter();
+      }
+    });
+    MotorBiometrico.getInstance();
+    MotorBiometrico.getInstance().getCliente().setMatchingThreshold(Integer.parseInt(is_matchingThreshold));
   }
 }
