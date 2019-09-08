0b56f5740c21270460b5ddd9cde081b7b1e0f478
Juan Angarita <juan.angarita@punchkickinteractive.com>
2019-09-08 11:10:49 -0500

    Fix logs

--- src/com/bachue/snr/biometrico/servicios/ws/BiometriaWS.java
+++ src/com/bachue/snr/biometrico/servicios/ws/BiometriaWS.java
@@ -67,7 +67,7 @@ public class BiometriaWS {
   @WebMethod(action = "consultarEstadisticas")
   @WebResult(name = "salidaEstadisticas")
   public EstadisticasSalidaDTO consultarEstadisticas(@WebParam(name = "entradaEstadisticas" ) EstadisticasEntradaDTO aeed_estadisticas) {
-    return iilb_logBusiness.consultarStats(aeed_estadisticas.getTipo(), aeed_estadisticas.getTipo());
+    return iilb_logBusiness.consultarStats(aeed_estadisticas.getTipo(), aeed_estadisticas.getIdEntidad());
   }
 
 
